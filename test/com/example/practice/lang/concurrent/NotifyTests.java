d4d220c97314af4636b39eb855905e1510fa5e5f
jerrylau <tenstone@foxmail.com>
2020-01-07 18:21:35 +0800

    准备cython JNI

--- test/com/example/practice/lang/concurrent/NotifyTests.java
+++
@@ -1,4 +0,0 @@
-package com.example.practice.lang.concurrent;
-
-public class NotifyTests {
-}
--- test/com/example/practice/lang/object/NotifyWaitTests.java
+++ test/com/example/practice/lang/object/NotifyWaitTests.java
@@ -1,4 +1,38 @@
 package com.example.practice.lang.object;
 
-public class NotifyWaitTests {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NotifyWaitTests {
+
+    private final Object lock = new Object();
+
+    {
+        synchronized (lock){
+            try {
+                lock.wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("NotifyAll")
+    synchronized void  NotifyAll() throws InterruptedException {
+        synchronized (lock) {
+            Thread thread = new Thread(() -> {
+                System.out.println("thread starting...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 对象解锁
+                lock.notifyAll();
+            });
+            thread.setDaemon(true);
+            thread.start();
+        }
+    }
 }
