582a0f17f0cfc68b4ced114d92ea96b27d5185cf
jerrylau <tenstone@foxmail.com>
2020-01-08 10:41:56 +0800

    update

--- test/com/example/practice/lang/datastruc/collections/StreamTests.java
+++ test/com/example/practice/lang/datastruc/collections/StreamTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * Stream API可以极大提高Java程序员的生产力，让程序员写出高效率、干净、简洁的代码。
@@ -23,5 +26,33 @@ class StreamTests {
         System.out.println(String.format("含有字符串的数量：%s", count));
     }
 
+    @Test
+    @DisplayName("打印随机数，遍历")
+    void PrintLimitString() {
+        Random random = new Random();
+        random.ints().limit(10).forEach(System.out::println);
+        System.out.println("======sorted=====");
+        random.ints().limit(10).sorted().forEach(System.out::println);
+    }
 
+
+    @Test
+    @DisplayName("统计整形数组中元素的最大值，最小值，平均值，个数，元素总和等等")
+    void statics(){
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        IntSummaryStatistics stats = numbers.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("列表中最大的数 : " + stats.getMax());
+        System.out.println("列表中最小的数 : " + stats.getMin());
+        System.out.println("所有数之和 : " + stats.getSum());
+        System.out.println("平均数 : " + stats.getAverage());
+    }
+
+    @Test
+    @DisplayName("顶点印务")
+    void output(){
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        // 获取对应的平方数
+        List<Integer> squaresList = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
+        System.out.println(squaresList);
+    }
 }
---
+++ test/com/example/practice/lang/function/LambdaTests.java
@@ -0,0 +1,14 @@
+package com.example.practice.lang.function;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LambdaTests {
+
+    @Test
+    @DisplayName("双冒号的用法")
+    void doubleQuota() {
+
+    }
+
+}
