02a4994f24b76bee597d0f684eb92b47ad4d00d8
jerrylau <tenstone@foxmail.com>
2020-01-08 10:03:29 +0800

    update

--- test/com/example/practice/lang/util/HttpTests.java
+++ test/com/example/practice/lang/util/HttpTests.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpConnectTimeoutException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * java.net.http 在java11开始支持
@@ -16,17 +19,59 @@ import java.time.Duration;
 class HttpTests {
 
     @Test
-    @DisplayName("http")
+    @DisplayName("同步http请求")
     void testSyncGet() throws IOException, InterruptedException {
         // 组建 request
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://openjdk.java.net/"))
                 .timeout(Duration.ofMillis(5009))
                 .build();
-
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+    }
+
+    @Test
+    @DisplayName("同步http请求，抛出异常")
+    void testSyncGetThrow() throws IOException, InterruptedException {
+        try{
+            // 组建 request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://openjdk.java.net/"))
+                    .timeout(Duration.ofMillis(1))
+                    .build();
+            HttpClient httpClient = HttpClient.newHttpClient();
+            httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        }catch (HttpConnectTimeoutException e){
+            System.out.println("HTTP connect timed out");
+        }
+        try{
+            // 组建 request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://openjdk.java.net/"))
+                    .timeout(Duration.ofMillis(10000))
+                    .build();
+            HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(1)).build();
+            httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        }catch (HttpConnectTimeoutException e){
+            System.out.println("HTTP connect timed out");
+        }
+    }
+
+    @Test
+    @DisplayName("异步http请求")
+    void testAsyncGet() throws IOException, InterruptedException, ExecutionException {
+        // 组建 request
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://openjdk.java.net/"))
+                .timeout(Duration.ofMillis(5009))
+                .build();
+        HttpClient httpClient = HttpClient.newHttpClient();
+        CompletableFuture future  = httpClient.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+        while (!future.isDone());
+        HttpResponse httpResponse = (HttpResponse) future.get();
+        System.out.println(httpClient.version());
+        System.out.println(String.format("返回body长度为：%s",httpResponse.body().toString().length()));
     }
 
 }
