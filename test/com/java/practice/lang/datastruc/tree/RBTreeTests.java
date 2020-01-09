f0744895b5d15d7ee80518290c167565dd63b6d3
jerrylau <tenstone@foxmail.com>
2020-01-09 18:31:20 +0800

    平衡二叉树 AVL树

---
+++ test/com/java/practice/lang/datastruc/tree/RBTreeTests.java
@@ -0,0 +1,10 @@
+package com.java.practice.lang.datastruc.tree;
+
+/**
+ * 应用广泛
+ *
+ * 红黑树一样也是平衡二叉搜索树，也是工业界最主要使用的二叉搜索平衡树。但平衡度红黑树没AVL那么好。
+ * 一般工业界把红黑树作为一种更通用的平衡搜索数来用，Java用它来实现TreeMap, C++的std::set/map/multimap，nginx中，用红黑树管理timer等
+ */
+public class RBTreeTests {
+}
