2c5179e67b18c0416c647ad549bf474bcd5c8596
jerrylau <tenstone@foxmail.com>
2020-01-09 12:36:25 +0800

    责任链设计模式

--- test/com/java/practice/patterns/behavioral/ChainOfResponsibilityPatternTests.java
+++ test/com/java/practice/patterns/behavioral/ChainOfResponsibilityPatternTests.java
@@ -3,6 +3,8 @@ package com.java.practice.patterns.behavioral;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Handler;
+
 /**
  * 顾名思义，责任链模式（Chain of Responsibility Pattern）为请求创建了一个接收者对象的链。
  * 这种模式给予请求的类型，对请求的发送者和接收者进行解耦。这种类型的设计模式属于行为型模式。
@@ -15,89 +17,175 @@ import org.junit.jupiter.api.Test;
  */
 class ChainOfResponsibilityPatternTests {
 
-    //常量
+    /**
+     * 常量  默认final
+     * 需要保持一个递+的关系
+     */
     interface Type {
-        int UI_EFFECT = 0;
-        int CODE_FORMAT = 1;
-        int TEST_PROJECT = 2;
-        int PUBLISH_APP = 3;
+        int UI_DESIGN = 2;
+        int CODE_DEVELOP = 3;
+        int CODE_TEST = 4;
+        int APP_PUBLISH = 5;
+        int REQUIREMENTS = 1;
     }
 
     //Handler（抽象处理者）
     abstract static class Handler {
-        private Handler mHandler;
 
-        Handler getmHandler() {
-            return mHandler;
+        protected int type = 0;
+
+        private Handler nextHandler;
+
+        Handler getNextHandler() {
+            return nextHandler;
         }
 
-        void setmHandler(Handler mHandler) {
-            this.mHandler = mHandler;
+        void setNextHandler(Handler nextHandler) {
+            this.nextHandler = nextHandler;
         }
 
+        /**
+         * 抽象方法，必须被重写
+         *
+         * @param type
+         * @param user
+         * @return
+         */
         abstract String handleRequest(int type, String user);
     }
 
-    //ConcreteHandler（具体处理者）
-    static class ConcreteHandlerGUI extends Handler {
+    /**
+     * 启动方法
+     */
+    static class TaskBoard {
+        Handler uiDesignHandler = new UIDesignHandler(Type.UI_DESIGN);
+        Handler codeDevelopHandler = new CodeDevelopHandler(Type.CODE_DEVELOP);
+        Handler codeTestHandler = new CodeTestHandler(Type.CODE_TEST);
+        Handler requirementsHandler = new RequirementsHandler(Type.REQUIREMENTS);
+        Handler appPublishHandler = new AppPublishHandler(Type.APP_PUBLISH);
+
+        /**
+         * 初始化
+         */
+        TaskBoard() {
+            requirementsHandler.setNextHandler(uiDesignHandler);
+            uiDesignHandler.setNextHandler(codeDevelopHandler);
+            codeDevelopHandler.setNextHandler(codeTestHandler);
+            codeTestHandler.setNextHandler(appPublishHandler);
+        }
+
+        /**
+         * 启动一个任务
+         *
+         * @param type
+         * @param user
+         */
+        void sendTask(int type, String user) {
+            System.out.println(String.format("----> User %s init a task", user));
+            this.requirementsHandler.handleRequest(type, user);
+        }
+    }
+
+    static class CodeDevelopHandler extends Handler {
+
+        CodeDevelopHandler(int type) {
+            this.type = type;
+        }
         @Override
         String handleRequest(int type, String user) {
             String ret = "";
-            if (Type.UI_EFFECT == type) {
-                ret = user + "’s App UI is Ok!";
+            if (this.type >= type) {
+                System.out.println("*********** CodeDevelopHandler **************");
+            }
+            if (getNextHandler() != null) {
+                ret = getNextHandler().handleRequest(type, user);
             } else {
-                if (getmHandler() != null) {
-                    System.out.println("*********** ConcreteHandlerGUI **************");
-                    ret = getmHandler().handleRequest(type, user);
-                }
+                System.out.println("chain exited.");
             }
             return ret;
         }
     }
 
-    static class ConcreteHandlerTeamLeader extends Handler {
+    static class UIDesignHandler extends Handler {
+
+        UIDesignHandler(int type) {
+            this.type = type;
+        }
+
         @Override
         String handleRequest(int type, String user) {
             String ret = "";
-            if (Type.CODE_FORMAT == type) {
-                ret = user + "’s App code format is Ok!";
+            if (this.type >= type) {
+                System.out.println("*********** UIDesignHandler **************");
+            }
+            if (getNextHandler() != null) {
+
+                ret = getNextHandler().handleRequest(type, user);
             } else {
-                if (getmHandler() != null) {
-                    System.out.println("*********** ConcreteHandlerTeamLeader **************");
-                    ret = getmHandler().handleRequest(type, user);
-                }
+                System.out.println("chain exited.");
             }
             return ret;
         }
     }
 
-    static class ConcreteHandlerPM extends Handler {
+    static class RequirementsHandler extends Handler {
+        RequirementsHandler(int type) {
+            this.type = type;
+        }
+
         @Override
         String handleRequest(int type, String user) {
+            if (this.type >= type) {
+                System.out.println("*********** RequirementsHandler **************");
+            }
             String ret = "";
-            if (Type.PUBLISH_APP == type) {
-                ret = user + "’s App can be publish!";
+            if (getNextHandler() != null) {
+
+                ret = getNextHandler().handleRequest(type, user);
             } else {
-                if (getmHandler() != null) {
-                    System.out.println("*********** ConcreteHandlerPM **************");
-                    ret = getmHandler().handleRequest(type, user);
-                }
+                System.out.println("chain exited.");
             }
             return ret;
         }
     }
 
-    static class ConcreteHandlerTestEngineer extends Handler {
+    static class CodeTestHandler extends Handler {
+        CodeTestHandler(int type) {
+            this.type = type;
+        }
+
         @Override
         String handleRequest(int type, String user) {
             String ret = "";
-            if (Type.TEST_PROJECT == type) {
-                ret = user + "’s App test is Ok!";
+            if (this.type >= type) {
+                System.out.println("*********** CodeTestHandler **************");
+            }
+            if (getNextHandler() != null) {
+
+                ret = getNextHandler().handleRequest(type, user);
             } else {
-                if (getmHandler() != null) {
-                    System.out.println("*********** ConcreteHandlerTestEngineer **************");
-                    ret = getmHandler().handleRequest(type, user);
-                }
+                System.out.println("chain exited.");
+            }
+            return ret;
+        }
+    }
+
+    static class AppPublishHandler extends Handler {
+
+        AppPublishHandler(int type) {
+            this.type = type;
+        }
+
+        @Override
+        String handleRequest(int type, String user) {
+            String ret = "";
+            if (this.type >= type) {
+                System.out.println("*********** AppPublishHandler **************");
+            }
+            if (getNextHandler() != null) {
+                ret = getNextHandler().handleRequest(type, user);
+            } else {
+                System.out.println("chain exited.");
             }
             return ret;
         }
@@ -107,17 +195,7 @@ class ChainOfResponsibilityPatternTests {
     @DisplayName("责任链模式")
     @Test
     void run() {
-        Handler code = new ConcreteHandlerTeamLeader();
-        Handler ui = new ConcreteHandlerGUI();
-        Handler test = new ConcreteHandlerTestEngineer();
-        Handler publish = new ConcreteHandlerPM();
-
-        code.setmHandler(ui);
-        ui.setmHandler(test);
-        test.setmHandler(publish);
-        //yanbo找team leader review代码
-        System.out.println(code.handleRequest(Type.CODE_FORMAT, "YanBo"));
-        //yanbo找team leader测试代码
-        System.out.println(code.handleRequest(Type.TEST_PROJECT, "YanBo"));
+        TaskBoard taskBoard = new TaskBoard();
+        taskBoard.sendTask(Type.REQUIREMENTS, "Yates");
     }
 }
