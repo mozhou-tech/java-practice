1d68c048ab759966b4970c493d38db4f0d1299ff
jerrylau <tenstone@foxmail.com>
2020-01-10 12:29:36 +0800

    graph首尾相接的对象

---
+++ test/com/java/practice/spring/core/beans/DependencyLookupTests.java
@@ -0,0 +1,9 @@
+package com.java.practice.spring.core.beans;
+
+/**
+ * 依赖查找(Dependency Lookup)：容器中的受控对象通过容器的API来查找自己所依赖的资源和协作对象。
+ * 这种方式虽然降低了对象间的依赖，但是同时也使用到了容器的API，造成了我们无法在容器外使用和测试对象。
+ * 依赖查找是一种更加传统的IoC实现方式。
+ */
+public class DependencyLookupTests {
+}
---
+++ test/com/java/practice/spring/core/beans/ReflectionTests.java
@@ -0,0 +1,7 @@
+package com.java.practice.spring.core.beans;
+
+/**
+ * 在Bean运行时，基于reflection机制可以动态修改，注入依赖
+ */
+public class ReflectionTests {
+}
