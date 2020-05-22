f52b55184261e7363ce28a3b0491a3aea1fca20e
liuyuancheng <tenstone@foxmail.com>
2020-05-22 23:12:49 +0800

    update

--- src/com/java/practice/leecode/PalindromeNumber9.java
+++ src/com/java/practice/leecode/PalindromeNumber9.java
@@ -31,13 +31,19 @@ public class PalindromeNumber9 {
     static class Solution {
         static boolean isPalindrome(int x) {
             if (x < 0) return false;
-
+            String s = String.valueOf(x);
+            int step = s.length();
+            for (int i = 0; i < step / 2; i++) {
+                if (s.charAt(i) != s.charAt(step - i -1)) {
+                    return false;
+                }
+            }
             return true;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(Solution.isPalindrome(-121));
+//        System.out.println(Solution.isPalindrome(-121));
         System.out.println(Solution.isPalindrome(112211));
     }
 }
